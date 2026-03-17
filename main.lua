b0f1e471b2b61390dc1c9a85481476d57ee973b3
Pavel Tikhonov <pt031192@gmail.com>
2026-03-17 19:35:27 +0200

    GFM BlightStorms script blocked

--- main.lua
+++ main.lua
@@ -1,2 +1,11 @@
 require("BlightStormInfection.weather") -- просто включает модуль уведомлений о смене погоды
 require("BlightStormInfection.timer") -- Главный файл. Проверки заражения работают по таймеру
+
+-- Блокируем скрипт BlightStorms из GFM
+local function blockBlightStormScript(e)
+    local script = "BlightStorms"
+    if tes3.getLegacyScriptRunning({ script = script }) then
+        tes3.stopLegacyScript({ script = script })
+    end
+end
+event.register("loaded", blockBlightStormScript)

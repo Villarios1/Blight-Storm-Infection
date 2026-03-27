6bd92dba33e2a7a2db61bb1947cff41a600e084d
Pavel Tikhonov <pt031192@gmail.com>
2026-03-27 16:04:05 +0200

    Improving readability. Added a hint about changing the language in the readme.

--- mcm.lua
+++ mcm.lua
@@ -11,10 +11,11 @@ local function registerModConfig()
         event.trigger("BlightStormInfection:UpdateTimer")
     end
 
+    -- 1. Базовые настройки
     local basePage = template:createSideBarPage({ label = "Базовые настройки" })
     local baseCategory = basePage:createCategory("Параметры")
 
-    -- 1. Базовый шанс
+    -- 1.1 Базовый шанс
     baseCategory:createSlider({
         label = "Базовый шанс заражения (0-100%)",
 		description = "Базовый шанс заражения персонажа моровой болезнью во время нахождения в моровой буре.",
@@ -25,7 +26,7 @@ local function registerModConfig()
         variable = mwse.mcm.createTableVariable{ id = "baseChance", table = config.base }
     })
 
-    -- 2. Множитель шлема
+    -- 1.2 Множитель шлема
     baseCategory:createSlider({
         label = "Множитель закрытого шлема",
         description = "Базовый шанс заражения будет умножен на это значение, если надет "..
@@ -39,7 +40,7 @@ local function registerModConfig()
         variable = mwse.mcm.createTableVariable{ id = "helmetMultiplier", table = config.base }
     })
 
-	-- 3. Интервал проверки
+	-- 1.3 Интервал проверки
     baseCategory:createSlider({
         label = "Интервал проверки в секундах",
         description = "Как часто скрипт проверяет шанс заражения.",
@@ -50,14 +51,14 @@ local function registerModConfig()
         variable = mwse.mcm.createTableVariable{ id = "duration", table = config.base }
     })
 
-	-- 4. Включение/выключение попыток заражения
+	-- 1.4 Включение/выключение попыток заражения
     baseCategory:createOnOffButton({
         label = "Отображение попыток заражения",
         description = "Отображать шанс и результат проверки на заражение при каждой попытке заразить персонажа.",
         variable = mwse.mcm.createTableVariable{ id = "displayInfectionAttempts", table = config.base }
     })
 
-	-- 5. Кнопка сброса настроек
+	-- 1.5. Кнопка сброса настроек
 	baseCategory:createButton({
         label = "Восстановить базовые настройки по умолчанию",
         buttonText = "Сбросить",
@@ -70,17 +71,17 @@ local function registerModConfig()
         end
     })
 
-    local weatherPage = template:createSideBarPage({ label = "Настройки погоды" })
+    -- 2. Оповещения о смене погоды
+    local weatherPage = template:createSideBarPage({ label = "Настройки оповещений" })
     local weatherCategory = weatherPage:createCategory("Параметры")
 
-    -- 1. Оповещения о погоде
     if not isWeatherModuleAvailable then
         weatherCategory:createInfo{
             label = "Модуль погоды не найден",
             description = "Настройки, связанные с оповещениями о погоде недоступны."
         }
     else
-        -- 1.1 Включение/выключение оповещений
+        -- 2.1 Включение/выключение оповещений
         weatherCategory:createYesNoButton({
             label = "Включить оповещения о моровой буре",
             description = "Показывать оповещение в нижней части экрана о начале и окончании моровой бури.",
@@ -94,21 +95,21 @@ local function registerModConfig()
             end
         })
 
-        -- 1.2 Текст оповещения о начале моровой бури
+        -- 2.2 Текст оповещения о начале моровой бури
         weatherCategory:createTextField({
             label = "Текст оповещения о начале моровой бури:",
             description = "Введите текст, который будет отображаться при начале моровой бури.",
             variable = mwse.mcm.createTableVariable{ id = "blightStormStartNotificationText", table = config.weather }
         })
 
-	    -- 1.3 Текст оповещения об окончании моровой бури
+	    -- 2.3 Текст оповещения об окончании моровой бури
 	    weatherCategory:createTextField({
             label = "Текст завершения бури:",
 		    description = "Введите текст, который будет отображаться при завершении моровой бури.",
             variable = mwse.mcm.createTableVariable{ id = "blightStormEndNotificationText", table = config.weather }
         })
 
-        -- 1.4 Кнопка сброса настроек
+        -- 2.4 Кнопка сброса настроек
 	    weatherCategory:createButton({
         label = "Восстановить настройки погоды по умолчанию",
         buttonText = "Сбросить",
